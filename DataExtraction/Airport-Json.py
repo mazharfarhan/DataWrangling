fec7055921efd6dc92c9cdde93bf4cfc608390c9
Farhan Mazhar <fmazhar@usc.edu>
2016-08-15 23:47:31 -0700

    Create Airport-Json.py

---
+++ DataExtraction/Airport-Json.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Complete the 'extract_airports' function so that it returns a list of airport
+codes, excluding any combinations like "All".
+"""
+
+from bs4 import BeautifulSoup
+html_page = "options.html"
+
+
+def extract_airports(page):
+    data = []
+    with open(page, "r") as html:
+        # do something here to find the necessary values
+        soup = BeautifulSoup(html, "lxml")
+        options = soup.find_all("option")
+        
+        for tag in options:
+            if (len(tag['value']) == 3  and tag['value'] != 'All' ):
+                data.append(tag['value']);
+
+    print len(data);
+
+    return data
+
+
+def test():
+    data = extract_airports(html_page)
+    assert len(data) == 15
+    assert "ATL" in data
+    assert "ABR" in data
+
+test()
